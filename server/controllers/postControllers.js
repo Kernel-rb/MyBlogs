b7715bdc9462af2380839c83b6b2eae80eac29e4
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-04 21:49:35 +0100

    fix bugs

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -4,39 +4,3 @@
 const createPost = async (req, res, next) => {
     res.json("Create Post 🦊 ")
 }
-// Get All Posts
-// Get: api/posts
-// UnProtected 
-const getPosts = async (req, res, next) => {
-    res.json("Get All Posts 🦊 ")
-}
-// Get a  Post by Category
-// Get: api/posts/
-// UnProtected
-const getPost = async (req, res, next) => {
-    res.json("Get A Single Post 🦊 ")
-}
-// Get Posts by category
-// Method: POST
-// UnProtected 
-const getCatPosts = async (req, res, next) => {
-    res.json("Get Posts by category 🦊 ")
-}
-// Create a Post
-// Method: POST
-// Protected 
-const createPost = async (req, res, next) => {
-    res.json("Create Post 🦊 ")
-}
-// Create a Post
-// Method: POST
-// Protected 
-const createPost = async (req, res, next) => {
-    res.json("Create Post 🦊 ")
-}
-// Create a Post
-// Method: POST
-// Protected 
-const createPost = async (req, res, next) => {
-    res.json("Create Post 🦊 ")
-}
