0e73bbbba471ffa26ab8a2339b404113704fb57c
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-09 21:49:37 +0100

    Refactor Header component and add logout functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,42 +1,62 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Logo from "..//images//logo.png"
-import {FaBars} from "react-icons/fa"
-import { AiOutlineClose } from "react-icons/ai"
-import { useState } from 'react'
-
+import React, { useState, useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from "../images/logo.png";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+import { UserContext } from '../context/userContext';
 
 function Header() {
-  const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false)
+  const [isNavShowing, setIsNavShowing] = useState(false);
+  const { currentUser } = useContext(UserContext);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsNavShowing(window.innerWidth > 800);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const closeNavHandler = () => {
-    if(window.innerWidth < 800) {
-      setIsNavShowing(false)
-    } else {
-      setIsNavShowing(true)
-    
-    }
-  }
+    setIsNavShowing(false);
+  };
+
+  const toggleNavHandler = () => {
+    setIsNavShowing(prevState => !prevState);
+  };
+
   return (
     <nav>
-      <div className="container  nav__container">
+      <div className="container nav__container">
         <Link to="/" className='nav__logo' onClick={closeNavHandler}>
-          <img src={Logo } alt="Logo" />
+          <img src={Logo} alt="Logo" />
         </Link>
-        {isNavShowing && <ul className='nav__menu'>
-          <li><Link to="/profile/kernel" onClick={closeNavHandler}> Profile</Link></li>
-          <li><Link to="/create"  onClick={closeNavHandler}>Create Post</Link></li>
-          <li><Link to="/authors"  onClick={closeNavHandler}>Authors</Link></li>
-          <li><Link to="/logout"  onClick={closeNavHandler}>Logout</Link></li>
-        </ul>
-        }
-        <button className="nav__toggle-btn" onClick={ () => setIsNavShowing(!isNavShowing)}>
-          {
-            isNavShowing ? <AiOutlineClose/> : <FaBars/>
-          }
+        {isNavShowing && (
+          <ul className='nav__menu'>
+            {currentUser ? (
+              <>
+                <li><Link to="/profile/kernel" onClick={closeNavHandler}>Profile</Link></li>
+                <li><Link to="/create" onClick={closeNavHandler}>Create Post</Link></li>
+                <li><Link to="/authors" onClick={closeNavHandler}>Authors</Link></li>
+                <li><Link to="/logout" onClick={closeNavHandler}>Logout</Link></li>
+              </>
+            ) : (
+              <>
+                <li><Link to="/authors" onClick={closeNavHandler}>Authors</Link></li>
+                <li><Link to="/login" onClick={closeNavHandler}>Login</Link></li>
+              </>
+            )}
+          </ul>
+        )}
+        <button className="nav__toggle-btn" onClick={toggleNavHandler}>
+          {isNavShowing ? <AiOutlineClose /> : <FaBars />}
         </button>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Header
+export default Header;
