5442095f684c36cd5d23a6de8ccc75252c5b3643
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-09 21:21:30 +0100

    Add axios library and update Register component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link , useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
   const [userData, setUserData] = useState({
@@ -10,18 +11,37 @@ const Register = () => {
     password2:'',
   })
 
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
+
+
   const changeInputHandler = (e) => {
     setUserData(prevState => {
       return {...prevState, [e.target.name]: e.target.value}
     })
   }
 
+  const registerUser = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/users/register`, userData)
+      const newUser = await res.data;
+      console.log(newUser)
+      if(!newUser) {
+        setError('Couldn\'t register user, please try again 🦊')
+      }
+      navigate('/login')
+    } catch (err) {
+      setError(err.response.data.message)
+    }
+  }
+
   return (
     <section className='register'>
       <div className="container">
         <h2>Sign Up</h2>
-        <form className="form register__form">
-          <p className="form__error-message">An error occurred 🦊 </p>
+        <form className="form register__form" onSubmit={registerUser}>
+          { error && <p className="form__error-message">{error}</p>}
           <input type="text" placeholder='Full Name ' name="name" value={userData.name} onChange={changeInputHandler} autoFocus />
           <input type="text" placeholder='Email  ' name="email" value={userData.email} onChange={changeInputHandler}  />
           <input type="password" placeholder='Password ' name="password" value={userData.password} onChange={changeInputHandler}  />
