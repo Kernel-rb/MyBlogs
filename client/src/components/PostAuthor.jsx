db61fbd997fc792d8114a435b9546f902a9a0fe3
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-10 15:58:54 +0100

    Add useEffect hook to fetch author data and update PostAuthor component

--- client/src/components/PostAuthor.jsx
+++ client/src/components/PostAuthor.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import Avatar from '../images/me.png'
+import axios from 'axios'
+import { useState } from 'react'
 
-const  PostAuthor = ({createdAt}) => {
+const PostAuthor = ({ authorID, createdAt }) => {
+    const [author, setAuthor] = useState({})
+    
+    useEffect(() =>
+    {
+        const getAuthor = async () =>
+        {
+            try
+            {
+                const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/users/${authorID}`);
+                setAuthor(res?.data);
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getAuthor();
+    }
+    , [])
+
   return (
       <Link to={`/posts/users/1`} className='post__author'>
           <div className="post__author-avatar">
-              <img src={Avatar} alt="" />
+              <img src={`${process.env.REACT_APP_ASSESTS_URL}/uploads/${author?.avatar}`} alt="" />
           </div>
           <div className="post__author-details">
               <h5>By : Saif Matab </h5>
